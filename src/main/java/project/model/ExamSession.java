12243753b07d9e5cfc6fe7e03cdf716c6ab3b06f
dunglt94 <tuandung9594@gmail.com>
2024-12-06 08:46:29 +0700

    add 6 models

--- src/main/java/project/model/ExamSession.java
+++ src/main/java/project/model/ExamSession.java
@@ -1,4 +1,41 @@
 package project.model;
 
+import java.time.LocalDate;
+
 public class ExamSession {
+    private String id;
+    private String name;
+    private LocalDate examDate;
+
+    public ExamSession() {}
+
+    public ExamSession(String id, String name, LocalDate examDate) {
+        this.id = id;
+        this.name = name;
+        this.examDate = examDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getExamDate() {
+        return examDate;
+    }
+
+    public void setExamDate(LocalDate examDate) {
+        this.examDate = examDate;
+    }
 }
